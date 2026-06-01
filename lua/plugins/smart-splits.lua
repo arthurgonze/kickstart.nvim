2bc53922cb9aa47e80332acbc74d130efb8f3afa
Arthur Gonze Machado <arthurgonze@gmail.com>
2026-06-01 10:12:21 -0400

    Adding wezterm support

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,17 +1,17 @@
 return {
   'mrjones2014/smart-splits.nvim',
-  keys = {
-    { '<C-h>', mode = 'n', desc = 'Move focus to the left window' },
-    { '<C-j>', mode = 'n', desc = 'Move focus to the lower window' },
-    { '<C-k>', mode = 'n', desc = 'Move focus to the upper window' },
-    { '<C-l>', mode = 'n', desc = 'Move focus to the right window' },
-    { '<A-h>', mode = 'n', desc = 'Resize split left' },
-    { '<A-j>', mode = 'n', desc = 'Resize split down' },
-    { '<A-k>', mode = 'n', desc = 'Resize split up' },
-    { '<A-l>', mode = 'n', desc = 'Resize split right' },
-  },
+  -- Wezterm Integration
+  lazy = false,
+  priority = 1000,
+
   config = function()
     local ss = require 'smart-splits'
+
+    ss.setup {
+      multiplexer_integration = 'wezterm',
+      default_amount = 3,
+      at_edge = 'wrap',
+    }
 
     -- Moving between splits
     vim.keymap.set('n', '<C-h>', ss.move_cursor_left, { desc = 'Move focus to the left window' })
